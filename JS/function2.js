a4808fe2cadd2cc62c760605eb684c069f9ec991
ChangNoi <44426331+RioRocker97@users.noreply.github.com>
2020-02-07 06:19:55 +0700

    Lazy coding

--- JS/function2.js
+++ JS/function2.js
@@ -1,7 +1,9 @@
 var car = 'Mitsubishi AE160'
 var country = 'Japan'
-
+var num
+num = 10
 function showcar(a,b){
     console.log('Here is my car. It is ' + a + '\nIt is made from ' + b)
 }
 showcar(car,country)
+console.log(isNaN(num))
