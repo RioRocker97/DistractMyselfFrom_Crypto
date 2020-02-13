c6c96164c880b32873ac101c2fe0716a10c04178
ChangNoi <44426331+RioRocker97@users.noreply.github.com>
2020-02-13 11:52:42 +0700

    JS 5

--- JS/es6destruct.js
+++ JS/es6destruct.js
@@ -1,5 +1,11 @@
 //ES6 destructiing is the way to declare variable in mutiple variable in one bracket []
 // This could be useful when i want to create complex obj.
+var myfood = {
+    seafood : "Squid",
+    junkfood : "chicken tendies",
+    cleanfood : "vegetable"
+}
+var [a,b,c] = [myfood.seafood,myfood.junkfood,myfood.cleanfood]
 const [first,second] = [true,false]
 let [food,car] = ['junk food','Mazda']
 var [grade,allowance,year] = [3.99,30000]
@@ -16,3 +22,7 @@ console.log(grade)
 console.log(year)
 year = 2020
 console.log(year)
+
+console.log(a)
+console.log(b)
+console.log(c)
