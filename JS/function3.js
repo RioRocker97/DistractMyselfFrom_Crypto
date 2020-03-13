1beb2ac6a187efa4f2412a10256b3f965ae8990a
ChangNoi <44426331+RioRocker97@users.noreply.github.com>
2020-03-13 17:42:34 +0700

    use underscore.js

--- JS/function3.js
+++ JS/function3.js
@@ -1,3 +1,4 @@
+const _ = require("underscore");
 function CreatemycarObject(n){
     return{
         name : n[0],
@@ -10,11 +11,24 @@ toyota = ['AE86','Toyota'];
 nissan = ['Fairlady Z','Nissan'];
 ford = ['Ford Mustang','Ford'];
 
+lambo = ['Lambogini Labadro','Lambogini'];
+mazda = ['Rx-8','Mazda'];
+benz = ['Mercedes Benz A900','Mercedes Benz'];
+
 var all_car_raw = [toyota,nissan,ford];
 var all_car = [];
 
+var all2_car_raw = [lambo,mazda,benz];
+var all2_car = [];
+
 all_car_raw.forEach(function(n){
-all_car.push(CreatemycarObject(n))
+    all_car.push(CreatemycarObject(n))
+});
+
+_.each(all2_car_raw,function(n){
+    all2_car.push(CreatemycarObject(n))
 });
 
 for(i=0;i<3;i++){all_car[i].moving()}
+console.log()
+for(i=0;i<3;i++){all2_car[i].moving()}
